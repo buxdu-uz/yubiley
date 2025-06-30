937e9463ebd06689d1ffa35ec32fe3cc4226895f
bekhruzsalimovsobirovich <bekhruzsalimovsobirovich@gmail.com>
2025-06-30 10:57:17 +0500

    confiigiration ready

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,35 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    /**
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<\Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -20,11 +44,66 @@ class Handler extends ExceptionHandler
 
     /**
      * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     * @throws Throwable
      */
-    public function register(): void
+    public function report(Throwable $exception): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        parent::report($exception);
+    }
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param Request $request
+     * @param Throwable $exception
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @throws Throwable
+     */
+    public function render($request, Throwable $exception): \Illuminate\Http\JsonResponse
+    {
+        return $this->handleException($request, $exception);
+    }
+    /**
+     * @param $request
+     * @param Throwable $exception
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function handleException($request, Throwable $exception): \Illuminate\Http\JsonResponse
+    {
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json('The specified method for the request is invalid', 405);
+        }
+
+        if ($exception instanceof RouteNotFoundException || $exception instanceof NotFoundHttpException) {
+            return response()->json('The specified URL cannot be found', 404);
+        }
+
+        if ($exception instanceof HttpException) {
+            return response()->json($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        if ($exception instanceof ValidationException) {
+
+            $items= $exception->validator->errors()->getMessages();
+            return response()->json($items, 422, $items);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return response()->json($exception->getMessage(), 401);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return response()->json($exception->getMessage(), 403);
+        }
+
+        return response()->json($exception->getMessage() . ' in ' . $exception->getFile() . ":" . $exception->getLine(), 500);
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * @param $message
+     * @param array $result
+     * @return JsonResponse
+     */
+    public function successResponse($message, $result = [])
+    {
+        $response = [
+            'status' => true,
+            'message' => $message,
+            'data'    => $result
+        ];
+        return response()->json($response);
+    }
+
+    /**
+     * Error response method.
+     *
+     * @param $message
+     * @param array $result
+     * @return JsonResponse
+     */
+    public function errorResponse($message, $result = []): JsonResponse
+    {
+        $response = [
+            'status' => false,
+            'error'=>true,
+            'message' => $message
+        ];
+
+        if (!empty($result)) {
+            $response['data'] = $result;
+        }
+
+        return response()->json($response);
+    }
 }
