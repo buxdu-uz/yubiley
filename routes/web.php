99221a9958fc65f18e405a2bbb8cd017ab42ca64
bekhruzsalimovsobirovich <bekhruzsalimovsobirovich@gmail.com>
2025-07-01 09:20:19 +0500

    dashboard template and auth full ready

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('front.index');
+})->name('main');
+
+Route::get('/login', [AuthController::class,'loginPage'])->name('auth.login-page');
+Route::post('/login', [AuthController::class,'login'])->name('auth.login');
+
+Route::group(['middleware' => ['auth:web']], function () {
+    Route::post('/logout', [AuthController::class,'logout'])->name('auth.logout');
+
+//    MAIN PAGE
+    Route::get('/main', [DashboardController::class,'index'])->name('dashboard');
+
+//    ADMIN SECTION
+    Route::group(['prefix' => 'admin'], function () {
+
+    });
+
+//    MANAGER SECTION
+    Route::group(['prefix' => 'manager'], function () {
+
+    });
+
+
 });
